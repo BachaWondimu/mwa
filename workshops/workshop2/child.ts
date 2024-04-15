7fb3fcd581eb41eaeb828b6b39271379dcf8bae1
Bacha Wondimu <bwondim3@montgomerycollege.edu>
2024-04-15 03:23:22 -0500

    readdir ans sharp have ben installed and implimented

--- workshops/workshop2/child.ts
+++ workshops/workshop2/child.ts
@@ -1,5 +1,22 @@
-process.on('message', (message) => {
-    if (process.send)
-      process.send("Hello from child process, Completed the work!");
-    process.exit(0);
-})
+import { readdir } from "node:fs/promises";
+import path from "node:path";
+
+import sharp from "sharp";
+
+process.on("message", async (message) => {
+  console.log(message);
+
+  const input = await readdir("./input");
+  const output = await readdir("./output");
+  const diff = input.filter((file) => !output.includes(file));
+  diff.forEach(async (file_name) => {
+    await sharp(`./input/{file_name}`)
+      .resize(200)
+      .toFile("output.webp", (err, info) => {});
+  });
+  console.log(diff);
+
+  if (process.send)
+    process.send("Hello from child process, Completed the work!");
+  process.exit(0);
+});
