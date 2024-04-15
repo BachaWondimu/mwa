0eafdeb00003a01ec10a46ae082fa570feb518a7
Bacha Wondimu <bwondim3@montgomerycollege.edu>
2024-04-15 11:26:51 -0500

    elastic mail has been installed and implemented

--- workshops/workshop2/child.ts
+++ workshops/workshop2/child.ts
@@ -2,6 +2,7 @@ import { readdir } from "node:fs/promises";
 import path from "node:path";
 
 import sharp from "sharp";
+import elasticMail from "elasticemail"
 
 process.on("message", async (message) => {
   console.log(message);
@@ -10,13 +11,32 @@ process.on("message", async (message) => {
   const output = await readdir("./output");
   const diff = input.filter((file) => !output.includes(file));
   diff.forEach(async (file_name) => {
-    await sharp(`./input/{file_name}`)
+    await sharp(`./input/${file_name}`)
       .resize(200)
-      .toFile("output.webp", (err, info) => {});
+      .toFile(`./output/${file_name}`);
   });
-  console.log(diff);
+    if (diff) {
+        var client = elasticMail.createClient({
+            username: 'baacaauumaa@gmail.com',
+            apiKey: 'FDC715315D100DBE02ED6BDAF6DA1CE0319D'
+        });
+ 
+        var msg = {
+            from: 'baacaauumaa@gmail.com',
+            from_name: 'Bacha Wondimu',
+            to: 'bwondimu@miu.edu',
+            subject: 'Hello',
+            body_text: diff
+        };
+ 
+        client.mailer.send(msg, function (err, result) {
+            if (err) {
+                return console.error(err);
+            }
+        })
 
-  if (process.send)
-    process.send("Hello from child process, Completed the work!");
-  process.exit(0);
+        if (process.send)
+            process.send("Hello from child process, Completed the work!");
+        process.exit(0);
+    }
 });
